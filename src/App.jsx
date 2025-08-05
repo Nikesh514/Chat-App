8229eec6f53780e743585915046424b8545740d9
Nikesh Mainali <nikeshmainali10@gmail.com>
2025-08-05 22:10:33 +0545

    first commit - new chat app

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import { Login } from './pages/Login/Login'
+import { Chat } from './pages/chat/chat'
+import ProfileUpdate from './pages/profileUpdate/ProfileUpdate'
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/chat' element={<Chat/>}/>
+        <Route path='/profile' element={<ProfileUpdate/>}/>
+      </Routes>
+    </>
+  )
+}
+
+export default App
