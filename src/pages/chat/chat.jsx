2572b0616c3522a9c3d9e18ea0b0729dd80f1412
Nikesh Mainali <nikeshmainali10@gmail.com>
2025-08-07 18:13:58 +0545

    created UI of leftsliderbar

--- src/pages/chat/chat.jsx
+++ src/pages/chat/chat.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './chat.css'
+import LeftSidebar from '../../components/LeftSidebar/LeftSidebar'
+import ChatBox from '../../components/ChatBox/ChatBox'
+import RightSidebar from '../../components/RightSidebar/RightSidebar'
 
 export const Chat = () => {
   return (
-    <div>
-      We are on the chat page
+    <div className='chat'>
+      <div className="chat-container">
+        <LeftSidebar />
+        <ChatBox/>
+        <RightSidebar />
+        
+      </div>
     </div>
   )
 }
