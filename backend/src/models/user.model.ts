655870650bdaa8b865f6b546008e902199bc6d77
Nikesh Mainali <nikeshmainali10@gmail.com>
2026-02-04 14:26:10 +0545

    added user model

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,45 +1,44 @@
-import mongoose, { Document } from 'mongoose';
+import mongoose, { Document } from "mongoose";
 
+export interface IUser extends Document {
+  firstName: string;
+  lastName: string;
 
-export interface IUser extends Document{
-    fullName: [
-        firstName: string,
-        lastName: string
-    ]
-    email: string;
-    password: string;
-    createdAt: Date;
-    updaftedAt: Date;
-    avatar?: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+  updaftedAt: Date;
+  avatar?: string;
 }
 
-const userSchema = new mongoose.Schema<IUser>({
-    fullName: {
-        firstName: {
-            type: String,
-            required: true,
-        },
-        lastName: {
-            type: String,
-            required: true,
-        },
+const userSchema = new mongoose.Schema<IUser>(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
     },
 
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
     password: {
-        type: String,
-        required: true,
-        minLength: [6, 'Password must be at least 6 characters long'],
+      type: String,
+      required: true,
+      minLength: [6, "Password must be at least 6 characters long"],
     },
-    avatar:{
-        type: String,
-    }
-},{timestamps: true});
+    avatar: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
-export const User = mongoose.model<IUser>('User', userSchema);
+export const User = mongoose.model<IUser>("User", userSchema);
